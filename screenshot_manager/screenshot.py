87cb3a45c37f06203b263ed30de6dceb01507e3a
Dzejkoubi <kubakraus06@gmail.com>
2024-05-28 11:24:50 +0200

    chore: Refactor screenshot manager and improve file naming

--- screenshot_manager/screenshot.py
+++ screenshot_manager/screenshot.py
@@ -5,14 +5,17 @@ from selenium.webdriver.chrome.options import Options
 import os
 
 
-def take_screenshot(url, output_file_name):
+def take_screenshot(url, output_directory):
+    # Replace "/" with "-" in the URL to save file correctly - when i saved it with / it wasnt saving the file correctly
+    filename = url.replace("/", "-").replace(":", "").replace("?", "").replace("=", "").replace("&", "") + ".png"
+    output_file_path = os.path.join(output_directory, filename)
+
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    chrome_options.add_argument("--headless")  # Chrome in headless mode
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")    
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("window-size=1920,1080")
-    
-    
+
     # Set up the WebDriver
     driver = webdriver.Chrome(service=ChromeService(), options=chrome_options)
 
@@ -20,22 +23,25 @@ def take_screenshot(url, output_file_name):
         # Open the webpage
         driver.get(url)
 
-        # Wait for the page to load completely
+        # Wait for the page to load completely (you can increase the wait time if needed)
         driver.implicitly_wait(10)
 
         # Take a screenshot and save it
-        driver.save_screenshot("images/" + "name.png" )
-        print(f"Screenshot saved as {"images/" + "name.png" }")
+        driver.save_screenshot(output_file_path)
+        print(f"Screenshot saved as {output_file_path}")
     finally:
         # Close the browser
         driver.quit()
 
 if __name__ == "__main__":
     # URL of the webpage to screenshot
-    url = "https://borndigital.ai/cs/"  # Replace with your URL
+    url = "https://borndigital.ai/cs/"  #Input URL
 
-    # Output file name
-    output_filename = "screenshot.png"
+    # Output directory
+    output_directory = "images"
+
+    # Ensure the output directory exists
+    os.makedirs(output_directory, exist_ok=True)
 
     # Take the screenshot
-    take_screenshot(url, output_filename)
+    take_screenshot(url, output_directory)
